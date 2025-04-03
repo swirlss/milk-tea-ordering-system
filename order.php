16a92bca1f825882157c794419dc8a4bc2d51105
swirlss <gajonluis@gmail.com>
2025-04-03 20:42:34 +0800

    Update order.php

--- order.php
+++ order.php
@@ -1,1 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "milktea_db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$flavor = $_POST['flavor'];
+$size = $_POST['size'];
+$toppings = $_POST['toppings'];
+
+$sql = "INSERT INTO orders (flavor, size, toppings) VALUES ('$flavor', '$size', '$toppings')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Order placed successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
 
