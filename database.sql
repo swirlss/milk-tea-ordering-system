e7d3b9e5dfca144a937807f37d133dff8d438112
swirlss <gajonluis@gmail.com>
2025-04-03 20:43:08 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE IF NOT EXISTS milktea_db;
+USE milktea_db;
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    flavor VARCHAR(50),
+    size VARCHAR(20),
+    toppings VARCHAR(50)
+);
 
